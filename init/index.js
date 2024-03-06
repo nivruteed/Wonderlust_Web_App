d69e0d4644e37fdf7e6e7cad4aa16fd76732c7ab
Nivrutee Dongare <dongarenivrutee8380@gmail.com>
2024-03-06 22:16:10 +0530

    basic part cover

---
+++ init/index.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+    .then(() => {
+        console.log("Connected to DB");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: '65e7ee3c387de3aff071d95e' }))
+    await Listing.insertMany(initData.data);
+    console.log("Data was initialized");
+}
+
+initDB();
